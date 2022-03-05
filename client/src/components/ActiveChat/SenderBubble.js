161375c04322f574c944bd04f7829a84ac0b0e91
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-03-05 08:32:55 -0800

    Added style for the avatar that is going to be display under the latest message

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Avatar } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,9 +25,15 @@ const useStyles = makeStyles(() => ({
     background: '#F4F6FA',
     borderRadius: '10px 10px 0 10px',
   },
+  avatar: {
+    height: 20,
+    width: 20,
+    marginLeft: 11,
+    marginTop: 6,
+  },
 }));
 
-const SenderBubble = ({ time, text }) => {
+const SenderBubble = ({LatestMessage,setShowAvatar, showAvatar,time, text, readStatus, otherUser, messageId }) => {
   const classes = useStyles();
 
   return (
